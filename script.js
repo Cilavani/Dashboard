79f7bce102fcf40fda5638715441a07ecce63105
Cilavani <86240154+Cilavani@users.noreply.github.com>
2024-09-04 19:01:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById('toggleBtn').addEventListener('click', function () {
     const sideNav = document.getElementById('sideNav');
+    const mainContent = document.getElementById('mainContent');
     if (sideNav.style.left === '-250px') {
         sideNav.style.left = '0';
+        mainContent.style.marginLeft = '250px';
     } else {
         sideNav.style.left = '-250px';
+        mainContent.style.marginLeft = '0';
     }
 });
